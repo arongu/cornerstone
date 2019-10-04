a544ed9e2f6901b28841c7ef7f2c534275e12fc6
Aron Gulyas <aron.gulyas.hu@gmail.com>
2019-10-04 09:41:49 +0200

    Snapshot commit main tool ...

---
+++ workflow/configtool/src/main/java/cornerstone/workflow/configtool/Configtool.java
@@ -0,0 +1,9 @@
+package cornerstone.workflow.configtool;
+
+import java.util.Arrays;
+
+public class Configtool {
+    public static void main(String[] args) {
+        System.out.println("args: " + Arrays.toString(args));
+    }
+}
