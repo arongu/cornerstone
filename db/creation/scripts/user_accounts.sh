09e39083c753842a4dc65278881feccb76c770e8
Aron Gulyas <aron.gulyas.hu@gmail.com>
2020-07-02 18:05:41 +0200

    creation script work in progress

--- db/creation/scripts/user_accounts.sh
+++ db/creation/scripts/user_accounts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# Generates the sql file to re-create the user_accounts database
+# DESTRUCTIVE IT WILL DESTROY THE PREVIOUS DB!
+
 # ----------------------------------------
 # Schemas
 # ----------------------------------------
-readonly schema_info='info'
+readonly schema_data='user_data'
 readonly schema_history='user_history'
 # ----------------------------------------
 # Prod
@@ -21,6 +25,8 @@ declare db_name=
 declare db_user=
 declare db_password=
 # ----------------------------------------
+declare SQL_FILE_NAME=
+# ----------------------------------------
 
 function setup_variables_for_prod() {
   local password="${1}"
@@ -29,6 +35,7 @@ function setup_variables_for_prod() {
   db_name="${db_prod_name}"
   db_user="${db_prod_user}"
   db_password="${password}"
+  SQL_FILE_NAME="create_${db_name}.sql"
 
   if [ -z "${db_name}" ]; then
     echo "'db_name' is not set for production!"
@@ -70,8 +77,8 @@ function setup_variables_for_test() {
   fi
 }
 
-function generate_drop_create_sql() {
-cat <<EOT >"01_drop_and_create_${db_name}.sql"
+function generate_sql_file() {
+cat <<EOT >"${SQL_FILE_NAME}"
 -- STEP 0 :: terminate all connections and drop database
 SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname='${db_name}';
 DROP DATABASE IF EXISTS ${db_name};
@@ -80,7 +87,7 @@ DROP DATABASE IF EXISTS ${db_name};
 -- STEP 1 :: create database and its schemas
 CREATE DATABASE ${db_name};
 \connect ${db_name};
-CREATE SCHEMA ${schema_info};
+CREATE SCHEMA ${schema_data};
 CREATE SCHEMA ${schema_history};
 --
 --
@@ -90,20 +97,21 @@ DROP SCHEMA public;
 EOT
 }
 
-
 case "${1}" in
   test)
     setup_variables_for_test "${2}"
-    generate_drop_create_sql
-  ;;
+    generate_sql_file
+    echo "psql -f ${SQL_FILE_NAME} (as postgres user)"
+    ;;
 
   prod)
     setup_variables_for_prod "${2}"
-    generate_drop_create_sql
-  ;;
+    generate_sql_file
+    echo "psql -f ${SQL_FILE_NAME} (as postgres user)"
+    ;;
 
   *)
     echo -e "Usage:\n${0} test|prod <db_password>"
     exit 10
-  ;;
+    ;;
 esac
