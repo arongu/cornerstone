a073fd4cef23c54d2dcc3b418dffb0a1441cdf74
Aron Gulyas <aron.gulyas.hu@gmail.com>
2021-02-06 18:25:54 +0100

    KeyRotatorImpl small refactor.

--- app/webapp/src/main/java/cornerstone/webapp/services/keys/rotation/KeyRotatorImpl.java
+++ app/webapp/src/main/java/cornerstone/webapp/services/keys/rotation/KeyRotatorImpl.java
@@ -1,8 +1,8 @@
 package cornerstone.webapp.services.keys.rotation;
 
-import cornerstone.webapp.logmsg.CommonLogMessages;
 import cornerstone.webapp.configuration.ConfigLoader;
 import cornerstone.webapp.configuration.enums.APP_ENUM;
+import cornerstone.webapp.logmsg.CommonLogMessages;
 import cornerstone.webapp.services.keys.stores.db.PublicKeyStore;
 import cornerstone.webapp.services.keys.stores.local.LocalKeyStore;
 import org.slf4j.Logger;
@@ -12,13 +12,15 @@ import javax.inject.Inject;
 import java.util.Properties;
 import java.util.Timer;
 
+/**
+ * Automatically rotates private and public keys in the local store.
+ */
 public class KeyRotatorImpl implements KeyRotator {
     private static final Logger logger = LoggerFactory.getLogger(KeyRotatorImpl.class);
 
     private final PublicKeyStore databasePublicKeyStore;
     private final LocalKeyStore localKeyStore;
     private final ConfigLoader configLoader;
-    private final Timer timer;
 
     @Inject
     public KeyRotatorImpl(final ConfigLoader configLoader,
@@ -29,7 +31,6 @@ public class KeyRotatorImpl implements KeyRotator {
         this.localKeyStore = localKeyStore;
         this.databasePublicKeyStore = dbPublicKeyStore;
 
-        timer = new Timer(getClass().getName());
         runRotationTask();
         logger.info(String.format(CommonLogMessages.MESSAGE_CONSTRUCTOR_CALLED, getClass().getName()));
     }
@@ -41,6 +42,9 @@ public class KeyRotatorImpl implements KeyRotator {
         final int rsaTTL               = Integer.parseInt(appProperties.getProperty(APP_ENUM.APP_RSA_TTL.key));
         final int jwtTTL               = Integer.parseInt(appProperties.getProperty(APP_ENUM.APP_JWT_TTL.key));
         final long period              = rsaTTL * 1000L;
-        timer.schedule(new KeyRotationTask(localKeyStore, databasePublicKeyStore, rsaTTL, jwtTTL, nodeName), 0, period);
+
+        final Timer timer = new Timer(getClass().getName());
+        final KeyRotationTask task = new KeyRotationTask(localKeyStore, databasePublicKeyStore, rsaTTL, jwtTTL, nodeName);
+        timer.schedule(task, 0, period);
     }
 }
