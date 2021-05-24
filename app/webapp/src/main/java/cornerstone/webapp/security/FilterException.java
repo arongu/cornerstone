fe81d793617437811e3092acab17b5d0fc4a167f
Aron Gulyas <aron.gulyas.hu@gmail.com>
2021-05-24 19:44:00 +0200

    pom and security updates

--- app/webapp/src/main/java/cornerstone/webapp/security/FilterException.java
+++
@@ -1,9 +0,0 @@
-package cornerstone.webapp.security;
-
-import io.jsonwebtoken.io.IOException;
-
-public class FilterException extends IOException {
-    public FilterException(String msg) {
-        super(msg);
-    }
-}
---
+++ app/webapp/src/test/java/cornerstone/webapp/security/AuthorizationFilterTest.java
@@ -0,0 +1,21 @@
+package cornerstone.webapp.security;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ResourceInfo;
+
+//@ExtendWith(MockitoJUnitRunner.class)
+public class AuthorizationFilterTest {
+//    @Mock
+//    private ResourceInfo resourceInfo;
+//
+//    @Mock
+//    private ContainerRequestContext containerRequestContext;
+//
+
+}
