d5f9e92c51234eac905d38774ccc65071ad3cef9
Aron Gulyas <aron.gulyas.hu@gmail.com>
2021-06-14 14:27:24 +0200

    First working annotation test for filter.

--- app/webapp/src/test/java/cornerstone/webapp/security/AuthorizationFilterTest.java
+++ app/webapp/src/test/java/cornerstone/webapp/security/AuthorizationFilterTest.java
@@ -1,79 +1,59 @@
 package cornerstone.webapp.security;
 
-/*
-@ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
-public class UserServiceUnitTest {
-
-    UserService userService;
-
-... //
-}
- */
-
-
 import cornerstone.webapp.services.accounts.management.UserRole;
 import io.jsonwebtoken.Claims;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.SecurityContext;
+import java.lang.reflect.Method;
 import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorizationFilterTest {
-    private static ResourceInfo resourceInfo;
-
-    // annotations through visualization :)
+    @RolesAllowed({"ADMIN"})
     @PermitAll
     public static class FilterMe {
-        private int called = 0;
-
         @DenyAll
-        public void run() {
-            called++;
-        }
+        public void toBeFiltered() {
 
-        public int getCalled() {
-            return called;
         }
-    }
-
-    @BeforeAll
-    public static void beforeAll() throws NoSuchMethodException {
-        resourceInfo = Mockito.mock(ResourceInfo.class);
-        Mockito.when(resourceInfo.getResourceMethod()).thenReturn(FilterMe.class.getMethod("run"));
-        Mockito.when(resourceInfo.getResourceClass().getAnnotations()).thenReturn(FilterMe.class.getAnnotations());
     }
 
     @Test
-    void test() {
+    void filter_shouldIgnorePermitAllAndShouldNotThrowException_whenRolesAllowedAnnotationIsSetToAdminAndUserRolesContainsAdmin() throws NoSuchMethodException {
+        // method, class initialization to avoid NullPointerException, for some reason it fails when Mockito does it
+        final Method method = FilterMe.class.getMethod("toBeFiltered");
+        final Class clazz   = FilterMe.class;
+
+        // mocking ResourceInfo
+        final ResourceInfo resourceInfo = Mockito.mock(ResourceInfo.class);
+        Mockito.when(resourceInfo.getResourceClass()).thenReturn(clazz);
+        Mockito.when(resourceInfo.getResourceMethod()).thenReturn(method);
         // prepare security context
         final boolean isSecure                = false;
         final Claims claims                   = null;
-        final Principal principal             = new PrincipalImpl("Legion");
+        final Principal principal             = new PrincipalImpl(UserRole.ADMIN.name());
         final Set<UserRole> userRoles         = new HashSet<>();
         userRoles.add(UserRole.USER);
         userRoles.add(UserRole.ADMIN);
+
+        // SecurityContext, ContainerRequestContext
+        final ContainerRequestContext containerRequestContext = Mockito.mock(ContainerRequestContext.class);
         final SecurityContext securityContext = new JwtSecurityContext(isSecure, principal, userRoles, claims);
-        // add security context to container request context
-        ContainerRequestContext containerRequestContext = Mockito.mock(ContainerRequestContext.class);
         Mockito.when(containerRequestContext.getSecurityContext()).thenReturn(securityContext);
 
 
         AuthorizationFilter authorizationFilter = new AuthorizationFilter(resourceInfo);
         authorizationFilter.filter(containerRequestContext);
-
     }
-
 }
