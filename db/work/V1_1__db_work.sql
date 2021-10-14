f274e4f0ebdfd1148d713a8d2a4eb7563f86059b
Aron Gulyas <aron.gulyas.hu@gmail.com>
2021-10-14 19:58:58 +0200

    Major update work db, removed schema variable.

--- db/work/V1_1__db_work.sql
+++ db/work/V1_1__db_work.sql
@@ -65,6 +65,6 @@ CREATE TRIGGER trigger_update_base64_key
 ----------------------------------------------------------------------------
 DROP ROLE IF EXISTS ${db.user};
 CREATE USER ${db.user} WITH ENCRYPTED PASSWORD '${db_password}';
-GRANT USAGE ON SCHEMA ${schema_secure} TO ${db_user};
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${schema_secure} TO ${db_user};
+GRANT USAGE ON SCHEMA secure TO ${db_user};
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA secure TO ${db_user};
 GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER ON secure.public_keys TO ${db_user};
