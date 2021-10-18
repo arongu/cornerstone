a82b3b364b796b7423daa99f5b89164cd2275e6d
Aron Gulyas <aron.gulyas.hu@gmail.com>
2021-10-18 14:24:23 +0200

    db users.sql updated

--- db/users/V1_1__db__users.sql
+++ db/users/V1_1__db__users.sql
@@ -3,66 +3,53 @@
 ----------------------------------------------------------------------------
 -- CREATION OF TABLE system.system_roles
 ----------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS system.system_roles(
-    id integer,
+CREATE TABLE IF NOT EXISTS system.roles(
+    id   integer,
     name varchar(20) NOT NULL,
     -- constraints
-    CONSTRAINT pkey_system_role_id PRIMARY KEY (id),
-    CONSTRAINT unique_system_role_name UNIQUE (name)
+    CONSTRAINT roles__role_id__pkey     PRIMARY KEY (id),
+    CONSTRAINT roles__role_name__unique UNIQUE (name)
 );
 
-INSERT INTO system.system_roles VALUES (0, 'NONE');
-INSERT INTO system.system_roles VALUES (1, 'USER');
-INSERT INTO system.system_roles VALUES (2, 'ADMIN');
+INSERT INTO system.roles VALUES (0, 'NO-ROLE');
+INSERT INTO system.roles VALUES (1, 'USER');
+INSERT INTO system.roles VALUES (2, 'ADMIN');
 ----------------------------------------------------------------------------
 -- END OF CREATION OF TABLE system.system_roles
 ----------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS system.user_roles(
-    id integer,
-    name varchar(20) NOT NULL,
-    -- constraints
-    CONSTRAINT pkey_user_role_id PRIMARY KEY (id),
-    CONSTRAINT unique_user_role_name UNIQUE (name)
-);
 
-INSERT INTO system.user_roles VALUES (0, 'NONE');
-INSERT INTO system.user_roles VALUES (1, 'READ');
-INSERT INTO system.user_roles VALUES (2, 'WRITE');
-----------------------------------------------------------------------------
--- END OF CREATION OF TABLE system.user_roles
 ----------------------------------------------------------------------------
 -- CREATION OF TABLE users.accounts
 ----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users.accounts(
-    account_id uuid NOT NULL DEFAULT gen_random_uuid(),
-    account_registration_ts timestamptz NOT NULL DEFAULT NOW(),
+    account_id                uuid                     NOT NULL DEFAULT gen_random_uuid(),
+    account_registration_ts   timestamptz              NOT NULL DEFAULT NOW(),
     -- account enable / disable
-    account_locked boolean NOT NULL,
-    account_locked_ts timestamptz NOT NULL DEFAULT NOW(),
-    account_lock_reason character varying(2048),
-    account_login_attempts integer NOT NULL DEFAULT 0,
+    account_locked            boolean                  NOT NULL,
+    account_locked_ts         timestamptz              NOT NULL DEFAULT NOW(),
+    account_lock_reason       character varying(2048),
+    account_login_attempts    integer                  NOT NULL DEFAULT 0,
     -- email address
-    email_address character varying(250) COLLATE pg_catalog."default" NOT NULL,
-    email_address_ts timestamptz NOT NULL DEFAULT NOW(),
+    email_address             character varying(250)   COLLATE pg_catalog."default" NOT NULL,
+    email_address_ts          timestamptz              NOT NULL DEFAULT NOW(),
     -- email address verification
-    email_address_verified boolean NOT NULL DEFAULT false,
+    email_address_verified    boolean                  NOT NULL DEFAULT false,
     email_address_verified_ts timestamptz,
     -- password change
-    password_hash character varying(128) NOT NULL UNIQUE,
-    password_hash_ts timestamptz NOT NULL DEFAULT NOW(),
-    --
-    system_role_id integer NOT NULL DEFAULT 1,
-    user_role_id   integer NOT NULL DEFAULT 1,
+    password_hash             character varying(128)   NOT NULL,
+    password_hash_ts          timestamptz              NOT NULL DEFAULT NOW(),
+    -- user role in the system
+    system_role_id            integer                  NOT NULL DEFAULT 1,
     -- constraints
-    CONSTRAINT pkey_account_id PRIMARY KEY (account_id),
-    CONSTRAINT uniq_email_address UNIQUE (email_address),
-    CONSTRAINT fg_key_system_role FOREIGN KEY(system_role_id) REFERENCES system.system_roles(id),
-    CONSTRAINT fg_key_user_role FOREIGN KEY(user_role_id) REFERENCES system.user_roles(id)
+    CONSTRAINT accounts__account_id__pkey       PRIMARY KEY (account_id),
+    CONSTRAINT accounts__email_address__unique  UNIQUE      (email_address),
+    CONSTRAINT accounts__password_hash__unique  UNIQUE      (password_hash),
+    CONSTRAINT accounts__system_role_id__fg_key FOREIGN KEY (system_role_id) REFERENCES system.roles(id)
 );
 
 -- indices
-CREATE INDEX IF NOT EXISTS index_email_address ON users.accounts(email_address);
-CREATE INDEX IF NOT EXISTS index_account_id ON users.accounts(account_id);
+CREATE INDEX IF NOT EXISTS account_id    ON users.accounts (account_id);
+CREATE INDEX IF NOT EXISTS email_address ON users.accounts (email_address);
 
 -- function  && trigger for account_enabled update
 CREATE OR REPLACE FUNCTION users.update_account_enabled_ts() RETURNS TRIGGER AS $$
@@ -72,8 +59,8 @@ CREATE OR REPLACE FUNCTION users.update_account_enabled_ts() RETURNS TRIGGER AS 
     END
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trigger_account_enabled ON users.accounts;
-CREATE TRIGGER trigger_account_enabled
+DROP   TRIGGER IF EXISTS account_enabled ON users.accounts;
+CREATE TRIGGER account_enabled
     BEFORE UPDATE OF account_locked ON users.accounts
     FOR EACH ROW
     EXECUTE PROCEDURE users.update_account_enabled_ts();
@@ -87,8 +74,8 @@ CREATE OR REPLACE FUNCTION users.update_email_address_ts() RETURNS TRIGGER AS $$
     END
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trigger_email_address ON users.accounts;
-CREATE TRIGGER trigger_email_address
+DROP   TRIGGER IF EXISTS email_address ON users.accounts;
+CREATE TRIGGER           email_address
     BEFORE UPDATE OF email_address ON users.accounts
     FOR EACH ROW EXECUTE PROCEDURE users.update_email_address_ts();
 -- end of email_address
@@ -101,8 +88,8 @@ CREATE OR REPLACE FUNCTION users.update_password_hash_ts() RETURNS TRIGGER AS $$
     END
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trigger_password_hash ON users.accounts;
-CREATE TRIGGER trigger_password_hash BEFORE UPDATE OF password_hash ON users.accounts
+DROP   TRIGGER IF EXISTS password_hash ON users.accounts;
+CREATE TRIGGER           password_hash BEFORE UPDATE OF password_hash ON users.accounts
 FOR EACH ROW EXECUTE PROCEDURE users.update_password_hash_ts();
 -- end of password_hash_last
 
@@ -114,14 +101,35 @@ CREATE OR REPLACE FUNCTION users.update_email_address_verified_ts() RETURNS TRIG
     END
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trigger_email_address_verified ON users.accounts;
-CREATE TRIGGER trigger_email_address_verified
+DROP  TRIGGER IF EXISTS email_address_verified ON users.accounts;
+CREATE TRIGGER          email_address_verified
     BEFORE UPDATE OF email_address_verified ON users.accounts
     FOR EACH ROW
     EXECUTE PROCEDURE users.update_email_address_verified_ts();
+----------------------------------------------------------------------------
 -- end of email_address_verified
 ----------------------------------------------------------------------------
 -- END OF CREATION OF TABLE users.accounts
+----------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
+-- CREATION OF TABLE users.account_http_method_permissions
+----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS users.account_http_method_permissions(
+    account_id uuid,
+    delete  boolean NOT NULL DEFAULT TRUE,
+    get     boolean NOT NULL DEFAULT TRUE,
+    head    boolean NOT NULL DEFAULT TRUE,
+    options boolean NOT NULL DEFAULT TRUE,
+    patch   boolean NOT NULL DEFAULT TRUE,
+    post    boolean NOT NULL DEFAULT TRUE,
+    put     boolean NOT NULL DEFAULT TRUE,
+    -- constraints
+    CONSTRAINT account_http_method_permissions__account_id__pkey  PRIMARY KEY (account_id),
+    CONSTRAINT account_http_method_permissions__account_id__fgkey FOREIGN KEY (account_id) REFERENCES users.accounts(account_id)
+);
+----------------------------------------------------------------------------
+-- END OF CREATION OF TABLE users.account_http_method_permissions
 ----------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------
@@ -130,6 +138,26 @@ CREATE TRIGGER trigger_email_address_verified
 DROP ROLE IF EXISTS ${db.user};
 CREATE USER ${db.user} WITH ENCRYPTED PASSWORD '${db_password}';
 ----------------------------------------------------------------------------
+-- END OF CREATION OF ROLES/USERS
+----------------------------------------------------------------------------
+
+
+
+----------------------------------------------------------------------------
+-- ____  _____ ____  __  __ ___ ____ ____ ___ ___  _   _ ____
+-- |  _ \| ____|  _ \|  \/  |_ _/ ___/ ___|_ _/ _ \| \ | / ___|
+-- | |_) |  _| | |_) | |\/| || |\___ \___ \| | | | |  \| \___ \
+-- |  __/| |___|  _ <| |  | || | ___) |__) | | |_| | |\  |___) |
+-- |_|   |_____|_| \_\_|  |_|___|____/____/___\___/|_| \_|____/
+----------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
+-- PERMISSIONS OF SCHEMA system
+----------------------------------------------------------------------------
+GRANT SELECT ON TABLE system.roles TO ${db.user};
+----------------------------------------------------------------------------
+-- END OF PERMISSIONS OF SCHEMA system
+----------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------
 -- PERMISSIONS OF SCHEMA users
@@ -137,14 +165,12 @@ CREATE USER ${db.user} WITH ENCRYPTED PASSWORD '${db_password}';
 GRANT USAGE ON SCHEMA users TO ${db.user};
 -- sequences, functions (usage is required to call nextval function)
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA users TO ${db.user};
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA users TO ${db.user};
-GRANT REFERENCES ON ALL TABLES IN SCHEMA users TO ${db.user};
+GRANT EXECUTE       ON ALL FUNCTIONS IN SCHEMA users TO ${db.user};
+GRANT REFERENCES    ON ALL TABLES    IN SCHEMA users TO ${db.user};
 
 -- TABLES
 GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER, REFERENCES ON TABLE users.accounts TO ${db.user};
-
+GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER, REFERENCES ON TABLE users.account_http_method_permissions TO ${db.user};
 ----------------------------------------------------------------------------
--- PERMISSIONS OF SCHEMA system
+-- END OF PERMISSIONS OF SCHEMA users
 ----------------------------------------------------------------------------
-GRANT SELECT ON TABLE system.system_roles TO ${db.user};
-GRANT SELECT ON TABLE system.user_roles TO ${db.user};
