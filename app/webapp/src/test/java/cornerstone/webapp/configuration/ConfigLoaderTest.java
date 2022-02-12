579f72a18e36c7c628e99b9e87b805710de6578d
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-02-12 14:58:56 +0100

    ConfigLoaderTest throws exception when TC is not properly set!

--- app/webapp/src/test/java/cornerstone/webapp/configuration/ConfigLoaderTest.java
+++ app/webapp/src/test/java/cornerstone/webapp/configuration/ConfigLoaderTest.java
@@ -9,23 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ConfigLoaderTest {
-    private static final String CONFIG_DIR = System.getenv("CONFIG_DIR");
-
-    private static final String keyFile  = Paths.get(CONFIG_DIR + "key.conf").toAbsolutePath().normalize().toString();
-    private static final String confFile = Paths.get(CONFIG_DIR + "app.conf").toAbsolutePath().normalize().toString();
-
     @Test
     public void constructor_shouldThrowIOException_whenFilesDoNotExist() {
         assertThrows(IOException.class, () -> new ConfigLoader("xxx", "xxx"));
     }
 
     @Test
-    public void constructorAndLoadAndDecryptConfig_shouldLoadConfig_whenAllSet() throws IOException {
-        final ConfigLoader cr = new ConfigLoader(keyFile, confFile);
+    public void constructorAndLoadAndDecryptConfig_shouldLoadConfig_whenAllSet() throws Exception {
+        final String CONFIG_DIR = System.getenv("CONFIG_DIR");
 
-        cr.loadAndDecryptConfig();
+        // throw exception if 'CONFIG_DIR' environment variable is not set
+        if ( CONFIG_DIR != null && ! CONFIG_DIR.isEmpty() ) {
+            final String keyFile  = Paths.get(CONFIG_DIR + "key.conf").toAbsolutePath().normalize().toString();
+            final String confFile = Paths.get(CONFIG_DIR + "app.conf").toAbsolutePath().normalize().toString();
 
-        assertEquals(keyFile, cr.getKeyFile());
-        assertEquals(confFile, cr.getConfFile());
+            final ConfigLoader cr = new ConfigLoader(keyFile, confFile);
+
+            cr.loadAndDecryptConfig();
+
+            assertEquals(keyFile, cr.getKeyFile());
+            assertEquals(confFile, cr.getConfFile());
+
+        } else {
+            throw new RuntimeException("Environment variable 'CONFIG_DIR' is not set for the test! to set it: export CONFIG_DIR=/home/aron/projects/cornerstone/_test_config/");
+        }
     }
 }
