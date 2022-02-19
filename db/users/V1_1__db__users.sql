eeff71e9b2eb413b17e864e2f4b5e7f9a5479808
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-02-19 23:21:28 +0100

    db/users updated

--- db/users/V1_1__db__users.sql
+++ db/users/V1_1__db__users.sql
@@ -4,23 +4,33 @@
 -- CREATION OF TABLE users.accounts
 ----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users.accounts
-(
+(   -- account type 'ADMIN / USER'
+    account_type              character varying(5)                                  NOT NULL,
     account_id                uuid                                                  NOT NULL,
+    account_owner_id          uuid                                                  NOT NULL,
     account_registration_ts   timestamptz                                           NOT NULL DEFAULT NOW(),
-    account_type              VARCHAR(5)                                            NOT NULL,
-
+    account_description       character varying(250),
     -- account enable / disable
     account_locked            boolean                                               NOT NULL DEFAULT false,
     account_locked_ts         timestamptz                                           NOT NULL DEFAULT NOW(),
     account_lock_reason       character varying(2048)                               NULL,
-
+    -- by default contact_email will be the owner of the account
+    contact_email             character varying(300)                                NOT NULL,
+    contact_phone             character varying(50),
+    -- if enabled multiple users can be assigned to the account
+    organization              boolean                                               NOT NULL DEFAULT  false,
     -- constraints
-    CONSTRAINT accounts__account_id__pkey       PRIMARY KEY (account_id),
-    CONSTRAINT accounts__account_type__check    CHECK (account_type in ('ADMIN', 'USER'))
+    CONSTRAINT accounts__account_id__pkey         PRIMARY KEY (account_id),
+    CONSTRAINT accounts__account_type__check      CHECK       (account_type in ('ADMIN', 'USER')),
+    -- 1 owner 1 account -- an email can only own 1 account
+    CONSTRAINT accounts__account_owner_id__uniq   UNIQUE      (account_owner_id),
+    CONSTRAINT accounts__contact_email__uniq      UNIQUE      (contact_email)
 );
 
 -- indices
-CREATE INDEX IF NOT EXISTS account_id    ON users.accounts (account_id);
+CREATE INDEX IF NOT EXISTS accounts__account_id    ON users.accounts (account_id);
+CREATE INDEX IF NOT EXISTS accounts__owner_id      ON users.accounts (account_owner_id);
+CREATE INDEX IF NOT EXISTS accounts__contact_email ON users.accounts (contact_email);
 
 -- function && trigger for account_enabled update
 CREATE OR REPLACE FUNCTION users.update_account_enabled_ts() RETURNS TRIGGER AS $$
@@ -42,12 +52,12 @@ CREATE TRIGGER account_enabled
 ----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users.account_members
 (
-    -- account where the this email address belongs
-    account_id                 uuid                                                  NOT NULL UNIQUE,
-    account_member_id          uuid                                                  NOT NULL,
-    account_member_locked      boolean                                               NOT NULL DEFAULT false,
-    account_member_locked_ts   timestamptz                                           NOT NULL DEFAULT NOW(),
-    account_member_lock_reason character varying(2048)                               NULL,
+    -- account where the this member belongs
+    account_id         uuid                                           NOT NULL UNIQUE,
+    member_id          uuid                                           NOT NULL,
+    member_locked      boolean                                        NOT NULL DEFAULT false,
+    member_locked_ts   timestamptz                                    NOT NULL DEFAULT NOW(),
+    member_lock_reason character varying(2048)                        NULL,
 
     -- email address
     email_address              character varying(250)   COLLATE pg_catalog."default" NOT NULL,
@@ -61,21 +71,26 @@ CREATE TABLE IF NOT EXISTS users.account_members
     password_hash_ts           timestamptz                                           NOT NULL DEFAULT NOW(),
 
     -- constraints
-    CONSTRAINT account_members__account_member_id__pkey PRIMARY KEY (account_member_id),
-    CONSTRAINT account_members__account_id__fg_key      FOREIGN KEY (account_id)       REFERENCES users.accounts(account_id),
+    CONSTRAINT account_members__account_member_id__pkey PRIMARY KEY (member_id),
+    CONSTRAINT account_members__account_id__fg_key      FOREIGN KEY (account_id)     REFERENCES users.accounts(account_id),
     CONSTRAINT account_members__email_address__unique   UNIQUE      (email_address),
     CONSTRAINT account_members__password_hash__unique   UNIQUE      (password_hash)
 );
+-- indices --
+CREATE INDEX IF NOT EXISTS account_members__account_id    ON users.account_members(account_id);
+CREATE INDEX IF NOT EXISTS account_members__member_id     ON users.account_members(member_id);
+CREATE INDEX IF NOT EXISTS account_members__email_address ON users.account_members(email_address);
+
+-- once account_members table created, the back reference for owner can be created
+ALTER TABLE users.accounts ADD CONSTRAINT accounts__owner_id__fgkey FOREIGN KEY (account_owner_id) REFERENCES users.account_members(member_id);
 ----------------------------------------------------------------------------
 -- END OF CREATION OF TABLE users.account_members
 ----------------------------------------------------------------------------
 -- CREATION OF TABLE users.http_method_permissions
 ----------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS users.account_permissions
+CREATE TABLE IF NOT EXISTS users.account_member_permissions
 (
-    account_id        uuid      NOT NULL,
-    account_member_id uuid      NOT NULL,
-
+    member_id         uuid      NOT NULL,
     -- http method permissions
     http_read         boolean, -- get, head, options
     http_write        boolean, -- patch, post, put
@@ -86,9 +101,10 @@ CREATE TABLE IF NOT EXISTS users.account_permissions
     account_admin     boolean,
 
     -- constraints
-    CONSTRAINT account_permissions__account_id__fg_key            FOREIGN KEY (account_id)          REFERENCES users.account_members(account_id),
-    CONSTRAINT account_permissions__account_member_id__fg_key     FOREIGN KEY (account_member_id)   REFERENCES users.account_members(account_member_id)
+    CONSTRAINT account_permissions__member_id__fg_key    FOREIGN KEY (member_id)   REFERENCES users.account_members(member_id)
 );
+-- indices --
+CREATE INDEX IF NOT EXISTS account_member_permission__member_id    ON users.account_member_permissions(member_id);
 
 ----------------------------------------------------------------------------
 -- CREATE ROLES/USERS
@@ -119,7 +135,7 @@ GRANT REFERENCES    ON ALL TABLES    IN SCHEMA users TO ${db_user};
 -- TABLES
 GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER, REFERENCES ON TABLE users.accounts TO ${db_user};
 GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER, REFERENCES ON TABLE users.account_members TO ${db_user};
-GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER, REFERENCES ON TABLE users.account_permissions TO ${db_user};
+GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER, REFERENCES ON TABLE users.account_member_permissions TO ${db_user};
 ----------------------------------------------------------------------------
 -- END OF PERMISSIONS OF SCHEMA users
 ----------------------------------------------------------------------------
