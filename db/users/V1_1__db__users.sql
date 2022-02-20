85e5b33ec9a2872181972b0ce853d2cb9c6e89c0
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-02-20 12:45:06 +0100

    db/users ++

--- db/users/V1_1__db__users.sql
+++ db/users/V1_1__db__users.sql
@@ -6,20 +6,20 @@
 CREATE TABLE IF NOT EXISTS users.groups
 (
     group_id                   uuid                                                  NOT NULL,
-    group_owner_id             uuid                                                  NOT NULL,
+    group_owner_id             uuid                                                  NOT NULL UNIQUE,
     group_name                 character varying(120),
     group_notes                character varying(4096),
-    registration_ts            timestamptz                                           NOT NULL DEFAULT NOW(),
+    group_creation_ts          timestamptz                                           NOT NULL DEFAULT NOW(),
     -- account enable / disable
     group_locked               boolean                                               NOT NULL DEFAULT false,
-    locked_ts                  timestamptz                                           NOT NULL DEFAULT NOW(),
-    lock_reason                character varying(2048)                               NULL,
+    group_locked_ts            timestamptz                                           NOT NULL DEFAULT NOW(),
+    group_lock_reason          character varying(2048)                               NULL,
+    group_locked_by            uuid,
     -- for multiple accounts
     max_users                  integer                                               NOT NULL DEFAULT 1,
     current_users              integer                                               NOT NULL DEFAULT 0,
     -- constraints
-    CONSTRAINT pkey__group_id  PRIMARY KEY (group_id),
-    CONSTRAINT uniq__user_id   UNIQUE      (group_owner_id)
+    CONSTRAINT pkey__group_id  PRIMARY KEY (group_id)
 );
 
 -- indices
@@ -29,7 +29,7 @@ CREATE INDEX IF NOT EXISTS group_owner_id ON users.groups(group_owner_id);
 -- functions && triggers
 CREATE OR REPLACE FUNCTION users.group_locked_ts() RETURNS TRIGGER AS $$
     BEGIN
-        NEW.locked_ts = NOW();
+        NEW.group_locked_ts = NOW();
         RETURN NEW;
     END
 $$ LANGUAGE plpgsql;
@@ -42,12 +42,14 @@ CREATE TRIGGER group_locked_ts
 ----------------------------------------------------------------------------
 -- END OF CREATION OF TABLE users.groups
 ----------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
 -- CREATION OF TABLE users.accounts
 ----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users.accounts
 (
     group_id                        uuid                                           NOT NULL UNIQUE,
-    id                              uuid                                           NOT NULL,
+    account_id                      uuid                                           NOT NULL UNIQUE,
     locked                          boolean                                        NOT NULL DEFAULT false,
     locked_ts                       timestamptz                                    NOT NULL DEFAULT NOW(),
     lock_reason                     character varying(2048)                        NULL,
@@ -60,24 +62,24 @@ CREATE TABLE IF NOT EXISTS users.accounts
     last_successful_login_ip_ts     timestamptz,
 
     -- email
-    email_address              character varying(250)   COLLATE pg_catalog."default" NOT NULL,
+    email_address              character varying(250)   COLLATE pg_catalog."default" NOT NULL UNIQUE,
     email_address_ts           timestamptz                                           NOT NULL DEFAULT NOW(),
     email_address_verified     boolean                                               NOT NULL DEFAULT false,
     email_address_verified_ts  timestamptz                                           NULL,
 
     -- password
-    password_hash              character varying(128)                                NOT NULL,
+    password_hash              character varying(128)                                NOT NULL UNIQUE,
     password_hash_ts           timestamptz                                           NOT NULL DEFAULT NOW(),
+    -- superuser
+    superuser                  boolean                                               NOT NULL DEFAULT false,
 
     -- constraints
-    CONSTRAINT pkey__id                PRIMARY KEY (id),
-    CONSTRAINT fkey__group_id          FOREIGN KEY (group_id)       REFERENCES users.groups(group_id),
-    CONSTRAINT uniq__email_address     UNIQUE      (email_address),
-    CONSTRAINT uniq__password_hash     UNIQUE      (password_hash)
+    CONSTRAINT pkey__id                PRIMARY KEY (account_id),
+    CONSTRAINT fkey__group_id          FOREIGN KEY (group_id)       REFERENCES users.groups(group_id)
 );
 -- indices --
 CREATE INDEX IF NOT EXISTS account_id    ON users.accounts(group_id);
-CREATE INDEX IF NOT EXISTS member_id     ON users.accounts(id);
+CREATE INDEX IF NOT EXISTS member_id     ON users.accounts(account_id);
 CREATE INDEX IF NOT EXISTS email_address ON users.accounts(email_address);
 
 -- functions && triggers
@@ -96,30 +98,36 @@ CREATE OR REPLACE FUNCTION users.last_successful_login_ip_ts() RETURNS TRIGGER A
 $$ LANGUAGE plpgsql;
 
 -- back reference group -> owner (back reference must be done with alter, because the two tables does not exist at creation time)
-ALTER TABLE users.groups ADD CONSTRAINT fkey__owner_id FOREIGN KEY (group_owner_id) REFERENCES users.accounts(id);
+ALTER TABLE users.groups ADD CONSTRAINT fkey__owner_id FOREIGN KEY (group_owner_id) REFERENCES users.accounts(account_id);
 ----------------------------------------------------------------------------
 -- END OF CREATION OF TABLE users.accounts
 ----------------------------------------------------------------------------
--- CREATION OF TABLE users.http_method_permissions
+
+----------------------------------------------------------------------------
+-- CREATION OF TABLE users.group_permissions
 ----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users.group_permissions
 (
-    group_id          uuid      NOT NULL,
-    -- group permissions, which group of functions/services can be used
-    -- this roughly translates to (http methods)
-    read         boolean        NOT NULL default true, -- get, head, options
-    write        boolean        NOT NULL default true, -- patch, post, put
-    delete       boolean        NOT NULL default true, -- delete
+    -- unique 1 account can only have 1 set of permission
+    account_id      uuid        NOT NULL UNIQUE,
+    -- group permissions, which group of functions/services can be used roughly translates to (http methods)
+    read            boolean     NOT NULL default true, -- get, head, options
+    write           boolean     NOT NULL default true, -- patch, post, put
+    delete          boolean     NOT NULL default true, -- delete
 
     -- group level permissions
     group_owner     boolean     NOT NULL default false,
     group_admin     boolean     NOT NULL default false,
 
     -- constraints
-    CONSTRAINT fkey__member_id FOREIGN KEY (group_id) REFERENCES users.accounts(id)
+    CONSTRAINT fkey__account_id FOREIGN KEY (account_id) REFERENCES users.accounts(account_id),
 );
 -- indices --
-CREATE INDEX IF NOT EXISTS member_id ON users.group_permissions(group_id);
+CREATE INDEX IF NOT EXISTS account_id ON users.group_permissions(account_id);
+----------------------------------------------------------------------------
+-- END OF CREATION OF TABLE users.group_permissions
+----------------------------------------------------------------------------
+
 
 ----------------------------------------------------------------------------
 -- CREATE ROLES/USERS
