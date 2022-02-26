90ecaa1146c5ea14b8b0ca998093dc3f2c8e91c6
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-02-26 02:59:40 +0100

    account triggers and functions

--- db/accounts/V1_1__db__accounts.sql
+++ db/accounts/V1_1__db__accounts.sql
@@ -3,138 +3,95 @@
 ----------------------------------------------------------------------------
 -- CREATION OF TABLE accounts.groups
 ----------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS accounts.groups
+CREATE TABLE IF NOT EXISTS accounts.account_groups
 (
-    group_id                   uuid                                                  NOT NULL,
-    group_id_ts                timestamptz                                           NOT NULL DEFAULT NOW(),
-    group_creation_ts          timestamptz                                           NOT NULL DEFAULT NOW(),
-    group_owner_id             uuid                                                  NOT NULL UNIQUE,
-    group_owner_id_ts          timestamptz                                           NOT NULL DEFAULT NOW(),
-    group_name                 character varying(120)                                NOT NULL UNIQUE,
-    group_name_ts              timestamptz                                           NOT NULL DEFAULT NOW(),
-    group_name_alt             character varying(120),
-    group_name_alt_ts          timestamptz,
-    group_notes                character varying(4096),
-    group_notes_ts             character varying(4096)                               NOT NULL DEFAULT NOW(),
+    account_group_created              timestamptz                                           NOT NULL DEFAULT NOW(),
+    account_group_id                   uuid                                                  NOT NULL,
+    account_group_owner_id             uuid                                                  NOT NULL UNIQUE,
+    account_group_owner_id_ts          timestamptz                                           NOT NULL DEFAULT NOW(),
+    account_group_name                 character varying(120)                                NOT NULL UNIQUE,
+    account_group_name_ts              timestamptz                                           NOT NULL DEFAULT NOW(),
+    account_group_name_alternative     character varying(120)                                UNIQUE,
+    account_group_name_alternative_ts  timestamptz,
+    account_group_notes                character varying(4096),
+    account_group_notes_ts             character varying(4096)                               NOT NULL DEFAULT NOW(),
 
     -- account enable / disable
-    group_locked               boolean                                               NOT NULL DEFAULT false,
-    group_locked_ts            timestamptz                                           NOT NULL DEFAULT NOW(),
-    group_lock_reason          character varying(2048),
-    group_lock_reason_ts       character varying(2048),
+    account_group_locked               boolean                                               NOT NULL DEFAULT false,
+    account_group_locked_ts            timestamptz                                           NOT NULL DEFAULT NOW(),
+    account_group_lock_reason          character varying(2048),
+    account_group_lock_reason_ts       character varying(2048),
 
     -- for multiple accounts
-    members_max                  integer                                             NOT NULL DEFAULT 1,
-    members_max_ts               timestamptz                                         NOT NULL DEFAULT NOW(),
-    members_current              integer                                             NOT NULL DEFAULT 1,
-    members_current_ts           timestamptz                                         NOT NULL DEFAULT NOW(),
+    account_group_max_members                  integer                                             NOT NULL DEFAULT 1,
+    account_group_max_members_ts               timestamptz                                         NOT NULL DEFAULT NOW(),
+    account_group_current_members              integer                                             NOT NULL DEFAULT 1,
+    account_group_current_members_ts           timestamptz                                         NOT NULL DEFAULT NOW(),
 
     -- constraints
-    CONSTRAINT pkey__group_id  PRIMARY KEY (group_id)
+    CONSTRAINT pkey__group_id  PRIMARY KEY (account_group_id)
 );
 
 -- indices
-CREATE INDEX IF NOT EXISTS group_id       ON accounts.groups(group_id);
-CREATE INDEX IF NOT EXISTS group_owner_id ON accounts.groups(group_owner_id);
+CREATE INDEX IF NOT EXISTS group_id       ON accounts.account_groups (account_group_id);
+CREATE INDEX IF NOT EXISTS group_owner_id ON accounts.account_groups (group_owner_id);
 
--- functions
--- group_id
-CREATE OR REPLACE FUNCTION accounts.groups__group_id_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.group_id_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+-- functions && triggers
+-- account_group_owner_id update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_owner_id_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_owner_id_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- group_owner_id
-CREATE OR REPLACE FUNCTION accounts.groups__group_owner_id_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.group_owner_id_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS update_account_group_owner_id_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_group_owner_id_ts BEFORE UPDATE OF account_group_owner_id ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_owner_id_ts();
 
--- group_name
-CREATE OR REPLACE FUNCTION accounts.groups__group_name_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.group_name_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+-- account_group_name update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_name_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_name_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- group_notes
-CREATE OR REPLACE FUNCTION accounts.groups__group_notes_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.group_notes_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS update_account_group_name_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_group_name_ts BEFORE UPDATE OF account_group_name ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_name_ts();
 
--- group_locked
-CREATE OR REPLACE FUNCTION accounts.groups__group_locked_ts() RETURNS TRIGGER AS $$
-    BEGIN
-        NEW.group_locked_ts = NOW();
-        RETURN NEW;
-    END
-$$ LANGUAGE plpgsql;
+-- account_group_notes update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_notes_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_notes_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- group_lock_reason
-CREATE OR REPLACE FUNCTION accounts.groups__group_lock_reason_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.group_lock_reason_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS update_account_group_notes_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_group_notes_ts BEFORE UPDATE OF account_group_notes ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_notes_ts();
 
--- members_current
-CREATE OR REPLACE FUNCTION accounts.groups__members_current_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.members_current_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+-- account_group_locked update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_locked_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_locked_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- members_max
-CREATE OR REPLACE FUNCTION accounts.groups__members_max_ts() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.members_max_ts = NOW();
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS update_account_group_locked_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_group_locked_ts BEFORE UPDATE OF account_group_locked ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_locked_ts();
 
--- triggers
--- group_id
-DROP TRIGGER IF EXISTS update_group_id ON accounts.groups;
-CREATE TRIGGER         update_group_id BEFORE UPDATE OF group_id ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__group_id_ts();
+-- account_group_lock_reason update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_lock_reason_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_lock_reason_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- group_owner_id
-DROP TRIGGER IF EXISTS update_group_owner_id ON accounts.groups;
-CREATE TRIGGER         update_group_owner_id BEFORE UPDATE OF group_owner_id ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__group_owner_id_ts();
+DROP TRIGGER IF EXISTS update_account_group_lock_reason_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_group_lock_reason_ts BEFORE UPDATE OF account_group_lock_reason ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_lock_reason_ts();
 
--- group_name
-DROP TRIGGER IF EXISTS update_group_name ON accounts.groups;
-CREATE TRIGGER         update_group_name BEFORE UPDATE OF group_name ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__group_name_ts();
+-- account_group_current_members update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_current_members_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_current_members_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- group_notes
-DROP TRIGGER IF EXISTS update_group_notes ON accounts.groups;
-CREATE TRIGGER         update_group_notes BEFORE UPDATE OF group_notes ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__group_notes_ts();
+DROP TRIGGER IF EXISTS update_account_current_members_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_current_members_ts BEFORE UPDATE OF account_group_current_members ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_current_members_ts();
 
--- group_locked
-DROP TRIGGER IF EXISTS update_group_locked ON accounts.groups;
-CREATE TRIGGER         update_group_locked BEFORE UPDATE OF group_locked ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__group_locked_ts();
+-- account_group_max_members_ts update timestamp on change
+CREATE OR REPLACE FUNCTION accounts.account_groups__account_group_max_members_ts() RETURNS TRIGGER AS $$
+BEGIN NEW.account_group_max_members_ts = NOW(); RETURN NEW; END $$ LANGUAGE plpgsql;
 
--- group_lock_reason
-DROP TRIGGER IF EXISTS update_group_lock_reason ON accounts.groups;
-CREATE TRIGGER         update_group_lock_reason BEFORE UPDATE OF group_lock_reason ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__group_lock_reason_ts();
-
--- members_current
-DROP TRIGGER IF EXISTS update_members_current ON accounts.groups;
-CREATE TRIGGER         update_members_current BEFORE UPDATE OF members_current ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__members_current_ts();
-
--- members_max
-DROP TRIGGER IF EXISTS update_members_max ON accounts.groups;
-CREATE TRIGGER         update_members_max BEFORE UPDATE OF members_max ON accounts.groups FOR EACH ROW EXECUTE PROCEDURE accounts.groups__members_max_ts();
-
+DROP TRIGGER IF EXISTS update_account_group_members_max_ts ON accounts.account_groups;
+        CREATE TRIGGER update_account_group_members_max_ts BEFORE UPDATE OF account_group_max_members ON accounts.account_groups
+            FOR EACH ROW EXECUTE PROCEDURE accounts.account_groups__account_group_max_members_ts();
 ----------------------------------------------------------------------------
 -- END OF CREATION OF TABLE accounts.groups
 ----------------------------------------------------------------------------
@@ -176,10 +133,10 @@ CREATE TABLE IF NOT EXISTS accounts.accounts
 
     -- constraints
     CONSTRAINT pkey__id             PRIMARY KEY (account_id),
-    CONSTRAINT fkey__group_id       FOREIGN KEY (group_id)  REFERENCES accounts.groups(group_id)
+    CONSTRAINT fkey__group_id       FOREIGN KEY (group_id)  REFERENCES accounts.account_groups(account_group_id)
 );
 -- back reference group -> owner (back reference must be done with alter, because the two tables does not exist at creation time)
-ALTER TABLE accounts.groups ADD CONSTRAINT fkey__owner_id FOREIGN KEY (group_owner_id) REFERENCES accounts.accounts(account_id);
+ALTER TABLE accounts.account_groups ADD CONSTRAINT fkey__owner_id FOREIGN KEY (group_owner_id) REFERENCES accounts.accounts(account_id);
 
 -- indices --
 CREATE INDEX IF NOT EXISTS account_id    ON accounts.accounts(account_id);
